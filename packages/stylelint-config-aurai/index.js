acd59f7d33f099eddc2797f02406df763142da0e
niuzhiwei3 <niuzhiwei3@jd.com>
2024-10-31 19:27:17 +0800

    chore: lint

--- packages/stylelint-config-aurai/index.js
+++ packages/stylelint-config-aurai/index.js
@@ -1,10 +1,19 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: ['stylelint-config-html', 'stylelint-config-recommended-vue', 'stylelint-config-standard', 'stylelint-config-prettier'],
   plugins: ['stylelint-order'],
   rules: {
     indentation: 4, //4空格
-    'selector-class-pattern':
-      '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
+   'selector-class-pattern': [
+      '^[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
+      {
+        /** This option will resolve nested selectors with & interpolation. - https://stylelint.io/user-guide/rules/selector-class-pattern/#resolvenestedselectors-true--false-default-false */
+        resolveNestedSelectors: true,
+        /** Custom message */
+        message: function expected(selectorValue) {
+          return `Expected class selector "${selectorValue}" to match BEM CSS pattern https://en.bem.info/methodology/css. Selector validation tool: https://regexr.com/3apms`;
+        },
+      },
+    ],
     // at-rule-no-unknown: 屏蔽一些scss等语法检查
     'at-rule-no-unknown': [true, { ignoreAtRules: ['mixin', 'extend', 'content', 'export', 'unocss-placeholder'] }],
     // css-next :global
